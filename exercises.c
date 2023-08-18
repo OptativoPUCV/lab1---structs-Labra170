48aa3b0a0ced69b994c5800522129495214e4c15
Replit user <>
2023-08-18 22:27:44 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -108,10 +108,10 @@ void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
     }
 
   
-  for (int i = 0; i < totalSize-1; i++)
+  /*for (int i = 0; i < totalSize-1; i++)
     {
       result[i] = arrAux[i];
-    }
+    }*/
 }
 
 /*
