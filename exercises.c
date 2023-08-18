41431e91319eeda6b23abd0bfbef283bdd7fc5b5
Replit user <>
2023-08-18 00:44:04 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -51,7 +51,7 @@ los números pares del arreglo original.
 */
 int *filterEvenNumbers(int arr[], int size, int *newSize) 
 { 
-  int *arrEven[size/2];
+  int *arrEven = (int *) calloc(size/2, sizeof(int));
   int pos = 0;
   
   for (int i = 0; i < size; i++)
